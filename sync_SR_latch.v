af0094f4084341590af4313b0901d654c4084776
vinamarora8 <vinam.arora8@gmail.com>
2017-03-09 00:19:34 +0530

    Adds Synchronous SR Latch module: sync_sr_latch

---
+++ sync_SR_latch.v
@@ -0,0 +1,22 @@
+module sync_SR_latch(clk, s, r, p, c, q);
+
+// Assigning ports as inputs/outputs
+input clk, s, r, p, c;
+output reg q;
+
+// Initiating registers
+initial
+	q = 1'b0;
+
+// SR action
+always @(posedge clk)
+begin
+	if (~p)
+		q <= 1'b1;
+	else if (~c)
+		q <= 1'b0;
+	else if (s)
+		q <= 1'b1;
+	else if (r)
+		q <= 1'b0;
+end
