6f121d5e71bff7ebeb5943a52ede99830ca3fe17
vinamarora8 <vinam.arora8@gmail.com>
2017-03-09 01:17:45 +0530

    Adds rotate_left_5b.v. Untested

---
+++ rotate_left_5b.v
@@ -0,0 +1,33 @@
+//// TODO
+// test
+
+module rotate_left_5b(din, S, clk, clr, dout);
+
+// Assigning ports as input/output
+input [4:0] din;
+input [2:0] S;
+input clk, clr;
+output reg [4:0] dout;
+
+// Initializing registers
+initial
+	dout <= din;
+
+// Rotate action:
+always @(posedge clk)
+begin
+	if (clr)
+		dout <= din;
+	else if (S == 3'd0)
+		dout <= din;
+	else if (S == 3'd1)
+		dout <= {din[3:0], din[4]};
+	else if (S == 3'd2)
+		dout <= {din[2:0], din[4:3]};
+	else if (S == 3'd3)
+		dout <= {din[1:0], din[4:2]};
+	else
+		dout <= {din[0], din[4:1]};
+end
+
+endmodule
