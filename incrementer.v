34ff02376ec3c830dc09c44a225075cb718d58cf
vinamarora8 <vinam.arora8@gmail.com>
2017-03-09 03:16:37 +0530

    Fixes bugs in incrementer.v. Tested. Works perfectly

--- incrementer.v
+++ incrementer.v
@@ -1,6 +1,3 @@
-//// TODO
-// test
-
 module incrementer(din, clk, clr, dout);
 
 // Parameter definition for varying bit-width
@@ -13,16 +10,16 @@ output reg [width-1 : 0] dout;
 
 // Initializing registers
 initial
-	dout <= {(width-1){1'b0}, 1'b1};
+	dout <= {{(width-1){1'b0}}, 1'b1};
 
 // Creating instantaneous incremented data
-wire dout_buf = din + {(width-1){1'b0}, 1'b1};
+wire [width-1 : 0] dout_buf = din + {{(width-1){1'b0}}, 1'b1};
 
 // Incrementer action:
 always @(posedge clk)
 begin
 	if (clr)
-		dout <= {(width-1){1'b0}, 1'b1};
+		dout <= {{(width-1){1'b0}}, 1'b1};
 	else
 		dout <= dout_buf;
 end
