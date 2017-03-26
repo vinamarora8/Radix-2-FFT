fdd36df01e33c03aa2067f14c7d7df7dbc9628fb
vinamarora8 <vinam.arora8@gmail.com>
2017-03-27 01:42:28 +0530

    Flips the selection of banks in memory_2_bank.v

--- memory_2_bank.v
+++ memory_2_bank.v
@@ -5,19 +5,20 @@ module memory_2_bank(clk, select, write_enable, addw_1, addw_2, addr_1, addr_2, 
 
 // Assigning ports as input/output
 input clk, select, write_enable;
-input [3:0] addw_1, addw_2, addr_1, addr_2;
+input [4:0] addw_1, addw_2, addr_1, addr_2;
 input [63:0] din_1, din_2;
 output [63:0] dout_1, dout_2;
 
-// Connections for memA
-wire [3:0] addA_1 = (select) ? addr_1 : addw_1;
-wire [3:0] addA_2 = (select) ? addr_2 : addw_2;
+//// Connections for memA
+// Write to A if select == 1
+wire [4:0] addA_1 = (select) ? addw_1 : addr_1;
+wire [4:0] addA_2 = (select) ? addw_2 : addr_2;
 wire [63:0] doutA_1, doutA_2;
 block_RAM bank1(
 	.clka(clk),
 	.clkb(clk),
-	.wea((~select)&(write_enable)),
-	.web((~select)&(write_enable)),
+	.wea((select)&(write_enable)),
+	.web((select)&(write_enable)),
 	.addra(addA_1),
 	.addrb(addA_2),
 	.dina(din_1),
@@ -26,15 +27,16 @@ block_RAM bank1(
 	.doutb(doutA_2)
 	);
 
-// Connections for memB
-wire [3:0] addB_1 = (select) ? addw_1 : addr_1;
-wire [3:0] addB_2 = (select) ? addw_2 : addr_2;
+//// Connections for memB
+// Write to B if select == 0
+wire [4:0] addB_1 = (select) ? addr_1 : addw_1;
+wire [4:0] addB_2 = (select) ? addr_2 : addw_2;
 wire [63:0] doutB_1, doutB_2;
 block_RAM bank2(
 	.clka(clk),
 	.clkb(clk),
-	.wea((select)&(write_enable)),
-	.web((select)&(write_enable)),
+	.wea((~select)&(write_enable)),
+	.web((~select)&(write_enable)),
 	.addra(addB_1),
 	.addrb(addB_2),
 	.dina(din_1),
